a0c0b1a75408b0c9d2ef982e37fbb09351512bd5
rafik <86847474+rafiksept@users.noreply.github.com>
2023-11-19 10:13:17 +0700

    Update getdataobat.py

--- getdataobat.py
+++ getdataobat.py
@@ -25,7 +25,7 @@ deksripsi_array = []
 isi_deksripsi_array = []
 
 
-for i in range(11000, 14000 ):
+for i in range(11000, 13000 ):
     name = df["name"][i]
     print(f"Index ke - {i} Ambil data - {name}")
     url = f"https://www.halodoc.com/obat-dan-vitamin/{name}"
